d83b1a4f5674cc48ae8399f05fae81b833546517
ravibalebilalu <81ravikiran@gmail.com>
2024-01-11 20:53:16 +0530

    html css

--- mypage/views.py
+++ mypage/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def my_view(request):
+    template = loader.get_template("my_view.html")
+    return HttpResponse(template.render())
+ 
